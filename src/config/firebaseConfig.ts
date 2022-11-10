b3a6f79d98811d28327871c2a7033a23e91004be
E.Sabathier <47697572+emi-sabathier@users.noreply.github.com>
2022-11-10 16:54:02 +0100

    fix: fix env properties names

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: process.env.FIRESTORE_API_KEY,
-    authDomain: process.env.FIRESTORE_AUTH_DOMAIN,
-    projectId: process.env.FIRESTORE_PROJECT_ID,
-    storageBucket: process.env.FIRESTORE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIRESTORE_MESSAGING_SENDER_ID,
-    appId: process.env.FIRESTORE_APP_ID,
-    measurementId: process.env.FIRESTORE_MEASUREMENT_ID,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 console.log(firebaseConfig)
 const app = initializeApp(firebaseConfig);
