cc24c8470bc58d94bc9ce246b525c25bd47dd70c
E.Sabathier <47697572+emi-sabathier@users.noreply.github.com>
2022-11-10 15:07:32 +0100

    fix: gitignore

---
+++ src/config/firebaseConfig.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: process.env.FIRESTORE_API_KEY,
+    authDomain: process.env.FIRESTORE_AUTH_DOMAIN,
+    projectId: process.env.FIRESTORE_PROJECT_ID,
+    storageBucket: process.env.FIRESTORE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIRESTORE_MESSAGING_SENDER_ID,
+    appId: process.env.FIRESTORE_APP_ID,
+    measurementId: process.env.FIRESTORE_MEASUREMENT_ID,
+};
+console.log(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
